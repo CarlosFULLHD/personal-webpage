1f5bb19bae307344fc7d2c72307e52cd36d7aca8
CarlosFULLHD <cninareynaga@gmail.com>
2024-03-31 11:24:46 -0400

    Añadiendo tooltips a autores

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import DetailedCard from "@/components/DetailedCard";
-import { Avatar, Button } from "@nextui-org/react";
+import { Avatar, Button, Tooltip } from "@nextui-org/react";
 import React, { useState, useEffect } from "react";
 import BonusCard from "@/components/BonusCard";
 import { AuthorsSection } from "@/components/AuthorsSection";
@@ -76,48 +76,90 @@ export default function Home() {
           </p>
 
           <div className="flex gap-4 items-center justify-center lg:absolute lg:left-48 lg:w-8/12 lg:z-10 xl:left-64 2xl:static 2xl:mx-auto 2xl:w-full">
-            <Avatar
-              isBordered
-              color="warning"
-              size="lg"
-              src="https://d1kdkr2pswehq6.cloudfront.net/jamesclear-photo.webp"
-              className="lg:w-20 lg:h-20 lg:text-large"
-            />
-            <Avatar
-              isBordered
-              color="warning"
-              size="lg"
-              src="https://d1kdkr2pswehq6.cloudfront.net/jordanpeterson-photo.webp"
-              className="lg:w-20 lg:h-20 lg:text-large"
-            />
-            <Avatar
-              isBordered
-              color="warning"
-              size="lg"
-              src="https://d1kdkr2pswehq6.cloudfront.net/tonyrobbins-img.webp"
-              className="lg:w-20 lg:h-20 lg:text-large"
-            />
-            <Avatar
-              isBordered
-              color="warning"
-              size="lg"
-              src="https://d1kdkr2pswehq6.cloudfront.net/calnewport-img.webp"
-              className="lg:w-20 lg:h-20 lg:text-large"
-            />
-            <Avatar
-              isBordered
-              color="warning"
-              size="lg"
-              className="absolute invisible md:static md:visible lg:w-20 lg:h-20 lg:text-large"
-              src="https://d1kdkr2pswehq6.cloudfront.net/ryanholiday-img.webp"
-            />
-            <Avatar
-              isBordered
-              color="warning"
-              size="lg"
-              className="absolute invisible md:static md:visible lg:w-20 lg:h-20 lg:text-large"
-              src="https://d1kdkr2pswehq6.cloudfront.net/viktorfrakl-img.webp"
-            />
+            <Tooltip
+              placement="bottom"
+              color="primary"
+              showArrow={true}
+              content="James Clear"
+            >
+              <Avatar
+                isBordered
+                color="warning"
+                size="lg"
+                src="https://d1kdkr2pswehq6.cloudfront.net/jamesclear-photo.webp"
+                className="lg:w-20 lg:h-20 lg:text-large"
+              />
+            </Tooltip>
+            <Tooltip
+              placement="bottom"
+              color="primary"
+              showArrow={true}
+              content="Jordan B. Peterson"
+            >
+              <Avatar
+                isBordered
+                color="warning"
+                size="lg"
+                src="https://d1kdkr2pswehq6.cloudfront.net/jordanpeterson-photo.webp"
+                className="lg:w-20 lg:h-20 lg:text-large"
+              />
+            </Tooltip>
+            <Tooltip
+              placement="bottom"
+              color="primary"
+              showArrow={true}
+              content="Tony Robbins"
+            >
+              <Avatar
+                isBordered
+                color="warning"
+                size="lg"
+                src="https://d1kdkr2pswehq6.cloudfront.net/tonyrobbins-img.webp"
+                className="lg:w-20 lg:h-20 lg:text-large"
+              />
+            </Tooltip>
+            <Tooltip
+              placement="bottom"
+              color="primary"
+              showArrow={true}
+              content="Cal Newport"
+            >
+              <Avatar
+                isBordered
+                color="warning"
+                size="lg"
+                src="https://d1kdkr2pswehq6.cloudfront.net/calnewport-img.webp"
+                className="lg:w-20 lg:h-20 lg:text-large"
+              />
+            </Tooltip>
+            <Tooltip
+              placement="bottom"
+              color="primary"
+              showArrow={true}
+              content="Ryan Holiday"
+            >
+              <Avatar
+                isBordered
+                color="warning"
+                size="lg"
+                className="absolute invisible md:static md:visible lg:w-20 lg:h-20 lg:text-large"
+                src="https://d1kdkr2pswehq6.cloudfront.net/ryanholiday-img.webp"
+              />
+            </Tooltip>
+            <Tooltip
+              placement="bottom"
+              color="primary"
+              showArrow={true}
+              content="Viktor Frankl"
+            >
+              <Avatar
+                isBordered
+                color="warning"
+                size="lg"
+                className="absolute invisible md:static md:visible lg:w-20 lg:h-20 lg:text-large"
+                src="https://d1kdkr2pswehq6.cloudfront.net/viktorfrakl-img.webp"
+              />
+            </Tooltip>
           </div>
           <div className="lg:h-96 bg-white lg:relative ">
             <div className="lg:absolute lg:left-64 lg:w-full lg:top-20 2xl:static 2xl:items-center">
--- cloudfront-loader.js
+++ cloudfront-loader.js
@@ -1,7 +1,6 @@
 // cloudfront-loader.js
 export default function cloudfrontLoader({ src, width, quality }) {
-  // Asume que 'src' ya incluye el path relativo dentro de tu bucket de S3
-  // y que tu distribución de CloudFront está configurada para servir ese bucket.
+  const format = quality && quality < 100 ? "webp" : "jpg";
   const qualityParam = quality ? `&q=${quality}` : "";
   return `https://d1kdkr2pswehq6.cloudfront.net/${src}?w=${width}${qualityParam}`;
 }
