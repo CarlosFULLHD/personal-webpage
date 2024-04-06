6e1849cdc4750b97aab933ee20e32a55ebe45d74
CarlosFULLHD <cninareynaga@gmail.com>
2024-04-06 15:16:53 -0400

    Feat: Adding modal for discount and promotion in 1min

--- components/ModalOneMinute.tsx
+++ components/ModalOneMinute.tsx
@@ -18,7 +18,7 @@ export default function ModalOneMinute() {
   useEffect(() => {
     const timer = setTimeout(() => {
       onOpen();
-    }, 60000); // Abre el modal después de 1 minuto
+    }, 6000); // Abre el modal después de 1 minuto
     return () => clearTimeout(timer);
   }, [onOpen]);
 
@@ -28,6 +28,22 @@ export default function ModalOneMinute() {
       section.scrollIntoView({ behavior: "smooth" });
     }
     onClose(); // Cierra el modal después de la acción
+  };
+
+  const [copySuccess, setCopySuccess] = useState("");
+
+  const discountcode = "LIBERTAD"; // Usa tu dirección de correo electrónico real aquí
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(discountcode).then(
+      () => {
+        setCopySuccess("¡Codigo de descuento copiado al portapapeles!");
+        setTimeout(() => setCopySuccess(""), 3000); // Limpiar mensaje después de 3 segundos
+      },
+      () => {
+        setCopySuccess("Error al copiar");
+      }
+    );
   };
 
   return (
@@ -42,15 +58,29 @@ export default function ModalOneMinute() {
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1 text-black text-center text-3xl">
+              <ModalHeader className="flex flex-col gap-1 text-black text-center text-3xl md:text-4xl">
                 ⏳ Oferta Exclusiva:
               </ModalHeader>
               <ModalBody className="text-center">
-                <p className=" text-black">
-                  ¡El tiempo se agota! Asegura tu acceso a Libertad Digital con
-                  bonificaciones especiales. Esta promoción termina en:
+                <p className=" text-black text-lg md:text-xl">
+                  Obtén acceso inmediato a "Libertad Digital" y sus Bonos
+                  Exclusivos. ¡Date prisa, la oferta termina en
                   <CountdownTimer />
                 </p>
+                <h1 className="font-bold text-black">Usa el código:</h1>
+                <h2 className="bg-custom-yellow py-2 rounded-lg text-black font-bold ">
+                  {discountcode}
+                </h2>
+                <h3 className="font-bold text-black">
+                  Recibe un{" "}
+                  <span className="underline decoration-4 decoration-custom-yellow">
+                    15% de descuento
+                  </span>{" "}
+                  en tu camino hacia la transformación.
+                </h3>
+                {copySuccess && (
+                  <h3 className="text-green-500">{copySuccess}</h3>
+                )}
               </ModalBody>
               <ModalFooter className="justify-center">
                 <Button
@@ -58,6 +88,9 @@ export default function ModalOneMinute() {
                   onClick={handleJoinNow}
                 >
                   Quiero unirme!
+                </Button>
+                <Button className="bg-custom-blue" onPress={copyToClipboard}>
+                  Copiar Código
                 </Button>
               </ModalFooter>
             </>
