1929587d263cbb0a7f81930d2646fab2b9172f8c
CarlosFULLHD <cninareynaga@gmail.com>
2024-05-18 22:53:54 -0400

    Recent Projects

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { Button, Input } from "@nextui-org/react";
+import RecentProjects from "@/components/sections/portfolio/RecentProjects";
 
 export default function Newsletter() {
   return (
     <>
       <h1>Porfolio web</h1>
+      <RecentProjects />
     </>
   );
 }
