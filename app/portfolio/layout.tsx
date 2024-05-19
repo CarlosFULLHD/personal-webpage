dc920beb288b2630d7acb9a7e75264cb472ec410
CarlosFULLHD <cninareynaga@gmail.com>
2024-05-18 22:43:02 -0400

    Portfolio web v1

---
+++ app/portfolio/layout.tsx
@@ -0,0 +1,11 @@
+export default function AboutLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <section className="">
+      <div className="">{children}</div>
+    </section>
+  );
+}
---
+++ app/portfolio/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Button, Input } from "@nextui-org/react";
+
+export default function Newsletter() {
+  return (
+    <>
+      <h1>Porfolio web</h1>
+    </>
+  );
+}
